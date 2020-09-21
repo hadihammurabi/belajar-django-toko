591146c26038fc6dc60b4ab8d5bd3c5600e84d4e
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-21 14:19:18 +0700

    read and create with rest api

--- barang/views.py
+++ barang/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect
+from django.forms.models import model_to_dict
+from django.http import HttpResponse
+import json
 
 from . import models
 
 def index(req):
   barangs = models.Barang.objects.all()
-  return render(req, 'barang/index.html', {
+  context = {
     'data': barangs,
-  })
+  }
+
+  if req.headers['Accept'] == 'application/json':
+    data = [model_to_dict(barang) for barang in barangs]
+    context['data'] = data
+    return HttpResponse(json.dumps(context))
+
+  return render(req, 'barang/index.html', context)
 
 def create(req):
   if req.POST:
-    models.Barang.objects.create(
+    barang = models.Barang.objects.create(
       nama=req.POST['nama'],
       harga=req.POST['harga']
     )
-    return redirect('/barang')
+    return HttpResponse(json.dumps({
+      'data': model_to_dict(barang),
+    }))
 
   return render(req, 'barang/create.html')
 
