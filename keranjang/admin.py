0bc84f4f17f4a95da05d188e24fb88f7d1956e15
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-22 11:10:06 +0700

    setup barang and keranjang admin

--- keranjang/admin.py
+++ keranjang/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+class KeranjangAdmin(admin.ModelAdmin):
+  pass
+
+admin.site.register(models.Keranjang, KeranjangAdmin)
